6cdbf69d830fcf241e0a84424070c23c8d87edf0
NidushaAmarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2022-02-10 01:56:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,23 @@ mark1.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https:
           telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/SlapTaps')),
 mark1.add(telebot.types.InlineKeyboardButton(text='➕Add To Group➕', url="https://t.me/NidushaOfficial_Bot?startgroup=true")),
 
+mark2 = telebot.types.InlineKeyboardMarkup()
+mark2.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https://t.me/SlapTap'),
+          telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/SlapTaps')),
+mark2.add(telebot.types.InlineKeyboardButton(text='🛠️Developer🛠️', url="https://t.me/NidushaAmarasinghe")),
+
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "<b>Hey There! This Is @NidushaAmarasinghe's Official Bot</b>\nJoin:-@SlapTap",parse_mode='Markdown', reply_markup=mark1)
+  bot.reply_to(message, "**Hey There! This Is @NidushaAmarasinghe's Official Bot</b>\nJoin:-@SlapTap**",parse_mode='Markdown', reply_markup=mark1)
    
 @bot.message_handler(commands=["help"])
 def send_message(message):
-  bot.send_message(message.chat.id, "Do You Want Help❔")
+  bot.send_message(message.chat.id, "Do You Want Help❔",parse_mode='Markdown', reply_markup=mark2)
  
 @bot.message_handler(commands=["alive"])
 def send_welcome(message):
-  bot.reply_to(message, "Hey There! Bot Online now. 💃🏻♥️\n**Developer:** @NidushaAmarasinghe\n**Official Website:www.nidushaofficial.tk**\nThank You For Using **Nidusha Official Bot**")
+  bot.reply_to(message, "Hey There! Bot Online now. 💃🏻♥️\nDeveloper:-@NidushaAmarasinghe\nOfficial Website:-www.nidushaofficial.tk\nThank You For Using Nidusha Official Bot")
  
 @bot.message_handler(commands=["about"])
 def send_welcome(message):
