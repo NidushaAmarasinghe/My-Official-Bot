6a030583bc6c85cbd0d020654fcf906241c6ea7a
NidushaAmarasinghe <95688464+NidushaAmarasinghe@users.noreply.github.com>
2022-02-11 22:32:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import telebot, requests, json
 from telebot import types
 from os import getenv
 
+bot = telebot.TeleBot(getenv("BOT_TOKEN"))
+
+
 # Markup
 mark1 = telebot.types.InlineKeyboardMarkup()
 mark1.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https://t.me/SlapTap'),
@@ -18,6 +21,8 @@ mark3.add(telebot.types.InlineKeyboardButton(text='🔁Updates🔁', url='https:
           telebot.types.InlineKeyboardButton(text='🧑‍💻Support🧑‍💻', url='https://t.me/SlapTaps')),
 mark3.add(telebot.types.InlineKeyboardButton('❔About❔',query.answer("Hi ",show_alert=true)),
 
+
+
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
   bot.reply_to(message, "**Hey There! This Is @NidushaAmarasinghe's Official Bot\nJoin:-@SlapTap**",parse_mode='Markdown', reply_markup=mark1)
@@ -32,18 +37,11 @@ def send_welcome(message):
  
 @bot.message_handler(commands=["about"])
 def send_welcome(message):
-  bot.reply_to(message, "📕 ᴀʙᴏᴜᴛ ᴍᴇ\n\n○ ᴍʏ ɴᴀᴍᴇ : Nidusha Official ʙᴏᴛ⚡️ (http://t.me/NidushaOfficial_Bot)\n○ ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ\n○ ғʀᴀᴍᴇᴡᴏʀᴋ: ᴘʏʀᴏɢʀᴀᴍ (https://docs.pyrogram.org/)\n○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ :🔐/n○ ᴠᴇʀsɪᴏɴ : 5.1\n○ ᴄʀᴇᴀᴛᴏʀ : @NidushaAmarasinghe\nJoin:-@SlapTap",parse_mode='Markdown', reply_markup=mark3)
-
-@bot.message_handler(commands=["pic"])
-def send_welcome(message):
-  await bot.reply_photo(
-       chat_id=message.chat.id,
-       photo='',
-       caption='hii'
+  bot.reply_to(message, "Hey There! This Is @NidushaAmarasinghe's Official Bot\nJoin:-@SlapTap",parse_mode='Markdown', reply_markup=mark3)
   
 @bot.message_handler(content_types=["photo", "sticker"])
-def send_content_message(message):
-    bot.send_photo(message.chat.id, "https://telegra.ph/file/87297d011cb91b4ec3014.jpg")
+def send_content_message(msg):
+    bot.reply_to(msg, "That's not a text message!")
     
 
     
